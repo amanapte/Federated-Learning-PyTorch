5cdbd3da1b43f1f7744b7f342607746689c636a8
Aman Apte <36478299+amanapte@users.noreply.github.com>
2021-07-13 16:39:42 +0530

    Update models.py
    Added NLP models.

--- src/models.py
+++ src/models.py
@@ -118,3 +118,89 @@ class modelC(nn.Module):
         pool_out.squeeze_(-1)
         pool_out.squeeze_(-1)
         return pool_out
+# NLP models.
+
+class TextClassificationModel(nn.Module):
+
+    def __init__(self, vocab_size, embed_dim, num_class):
+        super(TextClassificationModel, self).__init__()
+        self.embedding = nn.EmbeddingBag(vocab_size, embed_dim, sparse=True)
+        self.fc = nn.Linear(embed_dim, num_class)
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.5
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.fc.weight.data.uniform_(-initrange, initrange)
+        self.fc.bias.data.zero_()
+
+    def forward(self, text, offsets):
+        embedded = self.embedding(text, offsets)
+        return self.fc(embedded)
+    
+class LSTMClassifier(nn.Module):
+    
+    #define all the layers used in model
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, n_layers, 
+                 bidirectional, dropout):
+        
+        #Constructor
+        super().__init__()          
+        
+        #embedding layer
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        
+        #lstm layer
+        self.lstm = nn.LSTM(embedding_dim, 
+                           hidden_dim, 
+                           num_layers=n_layers, 
+                           bidirectional=bidirectional, 
+                           dropout=dropout,
+                           batch_first=True)
+        
+        #dense layer
+        self.fc = nn.Linear(hidden_dim * 2, output_dim)
+        
+        #activation function
+        self.act = nn.Sigmoid()
+        
+    def forward(self, text, text_lengths):
+        
+        #text = [batch size,sent_length]
+        embedded = self.embedding(text)
+        #embedded = [batch size, sent_len, emb dim]
+      
+        #packed sequence
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths,batch_first=True)
+        
+        packed_output, (hidden, cell) = self.lstm(packed_embedded)
+        #hidden = [batch size, num layers * num directions,hid dim]
+        #cell = [batch size, num layers * num directions,hid dim]
+        
+        #concat the final forward and backward hidden state
+        hidden = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1)
+                
+        #hidden = [batch size, hid dim * num directions]
+        dense_outputs=self.fc(hidden)
+
+        #Final activation function
+        outputs=self.act(dense_outputs)
+        
+        return outputs
+    
+class TextSentiment(nn.Module):
+    def __init__(self, vocab_size, embed_dim, num_class):
+        super().__init__()
+        self.embedding = nn.EmbeddingBag(vocab_size, embed_dim, sparse=True)
+        self.fc = nn.Linear(embed_dim, num_class)
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.5
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.fc.weight.data.uniform_(-initrange, initrange)
+        self.fc.bias.data.zero_()
+
+    def forward(self, text, offsets):
+        embedded = self.embedding(text, offsets)
+        return self.fc(embedded)
